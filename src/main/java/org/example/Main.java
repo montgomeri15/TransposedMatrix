303872fc0d2a0d7660377ec6cbfb309da02e782a
montgomeri15 <montgomeri15@gmail.com>
2022-11-05 13:33:53 +0200

    Transposed Matrix is ready for tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,13 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int row = 2;
-        int column = 3;
         int[][] basicMatrix = {{1, 2, 3}, {4, 5, 6}};
+        int row = basicMatrix.length;
+        int column = basicMatrix[0].length;
+        transposedMatrix(basicMatrix, row, column);
+    }
+
+    public static int[][] transposedMatrix(int[][] basicMatrix, int row, int column){
         displayMatrix(basicMatrix);
 
         int[][] transposedMatrix = new int[column][row];
@@ -14,6 +18,7 @@ public class Main {
             }
         }
         displayMatrix(transposedMatrix);
+        return transposedMatrix;
     }
 
     public static void displayMatrix(int[][] matrix) {
